60100b59f8600471396af806f4e1b63ba81fe1a4
PrithwiRaj Das <rupd6142@gmail.com>
2026-04-03 15:15:39 +0600

    Add files via upload

---
+++ Module 6/reversing linked list.cpp
@@ -0,0 +1,65 @@
+#include<bits/stdc++.h>
+using namespace std;
+   class Node
+   {
+   public:
+    int val;
+    Node *next;
+    Node(int val)
+   {
+       this->val=val;
+       this->next=NULL;
+          }
+          };
+   void insertattail(Node *&head,Node *&tail,int val)
+    {
+      Node* newnode=new Node(val);
+      if(head==NULL)
+      {
+          head=newnode;
+          tail=newnode;
+
+      }
+      tail->next=newnode;
+      tail=newnode;
+    }
+    void print(Node *tmp)
+    {
+        if(tmp==NULL){
+            return;
+        }
+        print(tmp->next);
+        cout<<tmp->val<<" ";
+    }
+
+   int main()
+{
+   Node *head=NULL;
+   Node *tail=NULL;
+    int val ;
+    while(true)
+    {
+        cin>>val;
+        if(val==-1)
+        {
+            break;
+        }
+        insertattail(head,tail,val);
+    }
+
+    print(head);
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
